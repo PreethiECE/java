6b70c41c442828a19507a8c1956990124e6c1b79
PreethiECE <preethiece96@gmail.com>
2016-08-20 13:15:06 +0530

    Create chck.java

---
+++ chck.java
@@ -0,0 +1,12 @@
+public static void main(String[] arg)
+{
+Scanner in=new Scanner(System.in);
+int n;
+n=in.nextInt();
+if(n>0)
+System.out.println(n+" is positive");
+else if(n==0)
+System.out.println(n+" is zero");
+else
+System.out.println(n+" is negative");
+}
